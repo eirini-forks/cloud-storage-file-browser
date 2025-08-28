48a942ed9ebd31312313e694bb27861de324d506
Danail Branekov <danailster@gmail.com>
2025-08-28 10:22:45 +0000

    Fix handling of REACT_APP_VCAP_SERVICES
    The bucket service was renamed to storage
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- app/src/config.js
+++ app/src/config.js
@@ -1,6 +1,6 @@
 export default {
-  APIEndpoint: JSON.parse(process.env.REACT_APP_VCAP_SERVICES).bucket[0].credentials.apiEndpoint,
-  BucketUrl: JSON.parse(process.env.REACT_APP_VCAP_SERVICES).bucket[0].credentials.bucketUrl,
+  APIEndpoint: JSON.parse(process.env.REACT_APP_VCAP_SERVICES).storage[0].credentials.apiEndpoint,
+  BucketUrl: JSON.parse(process.env.REACT_APP_VCAP_SERVICES).storage[0].credentials.bucketUrl,
   appName: 'Bucket File Manager', // The name that appears at the top of the app menu.
 }
 
