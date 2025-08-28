8a26f248d7b325dbf26692d73ea7b3d6956f02cb
Danail Branekov <danailster@gmail.com>
2025-08-28 15:48:43 +0000

    Make the script executable from everywhere
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- functions/build.sh
+++ functions/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pack build --builder=gcr.io/buildpacks/builder korifi/bucket-api:$(uuidgen)
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+pushd $SCRIPT_DIR
+{
+  pack build --builder=gcr.io/buildpacks/builder korifi/bucket-api:$(uuidgen)
+}
+popd
